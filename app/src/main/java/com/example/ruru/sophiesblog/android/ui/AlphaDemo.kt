21592b55d9a0a4090c144d916a993a4f42f3d08a
liangyaru <yaru.liang@eeoa.com>
2020-07-22 16:57:43 +0800

    新增-透明度Demo

---
+++ app/src/main/java/com/example/ruru/sophiesblog/android/ui/AlphaDemo.kt
@@ -0,0 +1,16 @@
+package com.example.ruru.sophiesblog.android.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.ruru.sophiesblog.R
+
+/**
+ * Created by lyr on 2020/6/5 & content is 透明度使用：alpha background十六进制
+ */
+class AlphaDemo: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.layout_alpha)
+  }
+}
