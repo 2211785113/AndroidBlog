8401cc978b0e3a172fb6d2168ff02c79e6b86d79
2211785113 <2211785113@qq.com>
2021-01-07 19:24:12 +0800

    新增- imageview 实现背景 小图平铺效果(不成)

---
+++ app/src/main/java/com/example/ruru/sophiesblog/android/ui/ImgTileAct.kt
@@ -0,0 +1,12 @@
+package com.example.ruru.sophiesblog.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ruru.sophiesblog.R
+
+class ImgTileAct: AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_img_tile)
+  }
+}
